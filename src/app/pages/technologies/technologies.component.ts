58f20f15619e4d8f01fc2841eb62dc61cae1a67a
Ricardo David Sigcha Sigcha <rsigcha@contraloria.gob.ec>
2024-04-30 10:20:46 -0500

    tercer commit

--- src/app/pages/technologies/technologies.component.ts
+++ src/app/pages/technologies/technologies.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-technologies',
-  templateUrl: './technologies.component.html',
-  styleUrls: ['./technologies.component.scss']
+  templateUrl: './technologies.component.html'
 })
 export class TechnologiesComponent implements OnInit {
 
