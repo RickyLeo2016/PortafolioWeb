5210884873223c59c68d3dab28dccdc73a1296e2
Ricardo David Sigcha Sigcha <rsigcha@contraloria.gob.ec>
2024-04-30 15:04:41 -0500

    cambios en diseño

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -2,14 +2,27 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  templateUrl: './contact.component.html'
 })
 export class ContactComponent implements OnInit {
 
+
+  test : Date = new Date();
+  focus;
+  focus1;
+
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit() {
+    let input_group_focus = document.getElementsByClassName('form-control');
+    let input_group = document.getElementsByClassName('input-group');
+    for (let i = 0; i < input_group.length; i++) {
+        input_group[i].children[0].addEventListener('focus', function (){
+            input_group[i].classList.add('input-group-focus');
+        });
+        input_group[i].children[0].addEventListener('blur', function (){
+            input_group[i].classList.remove('input-group-focus');
+        });
+    }
+}
 }
